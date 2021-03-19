24153aa7639af266b00f76c5526821ecb70f6a37
root <root@ip-172-31-48-80.ec2.internal>
2021-03-19 06:10:02 +0000

    docker

---
+++ job-DSL/nodejsdocker.groovy
@@ -0,0 +1,27 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/ponnu777/nodejs-project.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@angel')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('wardviaene/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
+
