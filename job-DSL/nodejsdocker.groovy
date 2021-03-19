a66046bf1f28c2fe25b6aac5bc36b97896d6af33
root <root@ip-172-31-48-80.ec2.internal>
2021-03-19 06:13:02 +0000

    ID

--- job-DSL/nodejsdocker.groovy
+++ job-DSL/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('wardviaene/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('angel95nr')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
